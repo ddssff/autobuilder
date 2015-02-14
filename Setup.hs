6d5070b3bcd64c36c8cb219f49c291bade71d6d1
David Fox <dsf@seereason.com>
2015-02-14 07:44:52 -0800

    Remove code to run test suite in Setup.hs, use runhaskell Setup test instead

--- Setup.hs
+++ Setup.hs
@@ -1,17 +1,5 @@
 #!/usr/bin/runhaskell
 
-import Control.Exception (SomeException)
 import Distribution.Simple
-import Distribution.Simple.LocalBuildInfo (LocalBuildInfo(buildDir))
-import System.Process (system)
-import System.Exit (ExitCode(ExitSuccess))
-import System.FilePath ((</>))
 
-main = defaultMainWithHooks simpleUserHooks {
-         postBuild = \ _ _ _ lbi -> runTestScript lbi
-       }
-
-runTestScript lbi =
-    -- system "runhaskell Test/Test.hs" >>= \ code ->
-    system (buildDir lbi </> "autobuilder-tests/autobuilder-tests") >>= \ code ->
-    if code == ExitSuccess then return () else error "Test Failure"
+main = defaultMainWithHooks simpleUserHooks
