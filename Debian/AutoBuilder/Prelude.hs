2197e01be6309690ccd1faeed068e6477ad6cd59
David Fox <dsf@seereason.com>
2017-07-22 07:57:21 -0700

    Add a replaceFile function to Debian.AutoBuilder.Prelude

--- Debian/AutoBuilder/Prelude.hs
+++ Debian/AutoBuilder/Prelude.hs
@@ -1,9 +1,19 @@
+{-# OPTIONS -Wall #-}
+
 module Debian.AutoBuilder.Prelude
     ( gFind
+    , replaceFile
     ) where
 
-import Data.Generics (Data, Typeable, listify)
+import Control.Exception (catch)
 import Control.Monad.Reader (MonadPlus, msum)
+import qualified Data.ByteString.Lazy as L (ByteString)
+import Data.Generics (Data, Typeable, listify)
+import Data.ListLike.IO (writeFile)
+import Data.ListLike.Instances ({-instance ListLikeIO L.ByteString Word8-})
+import Prelude hiding (writeFile)
+import System.Directory (removeFile)
+import System.IO.Error (isDoesNotExistError)
 
 -- | @gFind a@ will extract any elements of type @b@ from
 -- @a@'s structure in accordance with the MonadPlus
@@ -11,3 +21,15 @@ import Control.Monad.Reader (MonadPlus, msum)
 -- found while [Foo] will return the list of Foos found.
 gFind :: (MonadPlus m, Data a, Typeable b) => a -> m b
 gFind = msum . map return . listify (const True)
+
+-- Replace a file's contents, accounting for the possibility that the
+-- old contents of the file may still be being read.  Apparently there
+-- is a race condition in the file system so we may get one or more
+-- isAlreadyBusyError exceptions before the writeFile succeeds.
+replaceFile :: FilePath -> L.ByteString -> IO ()
+replaceFile path text =
+    --tries 100 10 f	-- There is now a fix for this problem, see ghc ticket 2122.
+    f
+    where
+      f :: IO ()
+      f = removeFile path `Control.Exception.catch` (\ e -> if isDoesNotExistError e then return () else ioError e) >> writeFile path text
